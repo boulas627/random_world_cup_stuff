900f20721036cd29160284313b269bee8e89a323
boulas627 <sebastien627@gmail.com>
2022-10-16 23:17:04 -0400

    Update world_cup_history.py

--- world_cup_history.py
+++ world_cup_history.py
@@ -23,43 +23,46 @@ def world_cup_losses(country_name, world_cup_df):
 
 	return countries_lost_to
 
+def world_cup_draws(country_name, world_cup_df): 
+	countries_drawn = []
+
+	for i in range(len(world_cup_df["outcome"])): 
+		if world_cup_df["outcome"][i] == "D": 
+
+			if world_cup_df["home_team"][i] == country_name: 
+				countries_drawn.append(world_cup_df["away_team"][i])
+
+			if world_cup_df["away_team"][i] == country_name:
+				countries_drawn.append(world_cup_df["home_team"][i])
+
+	return countries_drawn
+
+
 df = pd.read_csv("matches.csv")
 
 df = pd.DataFrame(df)
 
-print(df.columns)
+# print(df.columns)
 
-united_states_wins = 0
+# print(df.loc[200])
 
-for team in df['winning_team']: 
-	
-	if team == "United States": 
-		united_states_wins += 1
 
-print(united_states_wins)
+print("World Cup Draws")
+print(world_cup_draws("United States", df))
 
-countries_defeated = []
+print("World Cup Losses")
+print(world_cup_losses("United States", df))
 
-for i in range(len(df["winning_team"])): 
-
-	# print(df["losing_team"][i])
-	if df["winning_team"][i] == "United States": 
-		countries_defeated.append(df["losing_team"][i])
-		# print(df["losing_team"][i])
+print("World Cup Victories")
+print(world_cup_victories("United States", df))
 
 
 
-print(world_cup_losses("Senegal", df))
-
-sys.exit()
-
-
-match_results = ["Win", "Loss", "Draw"]
+# match_results = ["Win", "Loss", "Draw"]
  
-for row in df.iterrows(): 
-	print(row)
-	# if row["home_team"] == "United States" or row["away_team"] == "United States": 
-	# 	print("I")
+# for row in df.iterrows(): 
+# 	print(row)
+	
 
 
 
