0af4d3f73f542528183329680c51aff8f9a73227
boulas627 <sebastien627@gmail.com>
2022-10-31 21:35:30 -0400

    Update goals_per_game.py

--- goals_per_game.py
+++ goals_per_game.py
@@ -9,8 +9,6 @@ df = pd.DataFrame(csv_data)
 
 print(df["goals_scored"])
 
-AWS_ACCESS_KEY = "SWEF4634667"
-
 goals_per_game = []
 
 for i in range(len(df)): 
@@ -19,3 +17,11 @@ for i in range(len(df)):
 
 
 print(goals_per_game)
+
+df["goals_per_game"] = goals_per_game
+
+plt.plot(df["year"], df["goals_per_game"])
+plt.title("Goals Per Game In Each World Cup")
+plt.xlabel('World Cup Year')
+plt.ylabel('Goals Per Game')
+plt.show()
